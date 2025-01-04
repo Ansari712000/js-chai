c88dd2bcc88c478cb63eba4340a86d56370639a9
Ansari712000 <59525138+Ansari712000@users.noreply.github.com>
2025-01-04 11:59:28 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+ let name = "hitesh"
+ let age = 24
+ let isloggedin = false
+
+ //number => 2 to the power 53
+ //bigint 
+ //string =>""
+ //boolean => true/false
+ //null => standalone value
+ //undefined =>
+//symbol => unique
+ //object
+
+ console.log(typeof null); //object
+ console.log(typeof undefined); //undefined
